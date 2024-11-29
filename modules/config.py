de0677f05c93dbcebfec3634481ab62a3c87a112
Silver Phoenix Ideas <silver.phoenix.ideas@gmail.com>
2024-11-29 00:56:55 -0500

    Implement functionality for sending emails from the contact form.

--- modules/config.py
+++ modules/config.py
@@ -1,1 +1,3 @@
 APP_TITLE = "Project Showcase App"
+APP_EMAIL_HOST = "smtp.gmail.com"
+APP_EMAIL_PORT = 465
--- pages/contact.py
+++ pages/contact.py
@@ -2,6 +2,7 @@
 import streamlit as st
 import modules.config as config
 import modules.components as components
+import modules.email_helper as email_helper
 
 # Configurations
 st.set_page_config(layout="wide", page_title="Contact Me | " + config.APP_TITLE)
@@ -11,6 +12,14 @@ components.page_navigation()
 st.title("Contact Me")
 
 with st.form(key="contact_form"):
-    st.text_input("Your Email Address:")
-    st.text_area("Your Message:")
-    st.form_submit_button("Send Message")
+    sender = st.text_input("Your Email Address:")
+    message = st.text_area("Your Message:")
+    submit = st.form_submit_button("Send Message")
+
+if submit:
+    status = st.info("Sending...")
+    try:
+        email_helper.send_email(sender, message)
+        status.info("Your message was sent successfully.")
+    except:
+        status.error("Your message could not be sent.")
